00a18f42a3d27f18e3445051380b8f8bde8d49c0
Abelard Bautista <abelardbautista@gmail.com>
2020-05-28 23:46:16 -0700

    Moved page routing functionality to Navigation.js.

--- src/App.js
+++ src/App.js
@@ -1,55 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navigation from "./components/Navigation";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Resume from "./pages/Resume";
 
 export default function App() {
   return (
     <Router>
       <Navigation />
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/About">About</Link>
-            </li>
-            <li>
-              <Link to="/Projects">Projects</Link>
-            </li>
-            <li>
-              <Link to="/Resume">Resume</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/About">
-            <About />
-          </Route>
-          <Route path="/Projects">
-            <Projects />
-          </Route>
-          <Route path="/Resume">
-            <Resume />
-          </Route>
-        </Switch>
-      </div>
     </Router>
   );
 }
-
-// function Home() {
-//   return <h2>Home</h2>;
-// }
-
-// function About() {
-//   return <h2>About</h2>;
-// }
-
-// function Users() {
-//   return <h2>Users</h2>;
-// }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,33 +1,50 @@
 import React from "react";
+import { BrowserRouter as Switch, Route, Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import About from "../pages/About";
+import Projects from "../pages/Projects";
+import Resume from "../pages/Resume";
 // import NavDropdown from "react-bootstrap/NavDropdown";
 
 const Navigation = () => {
   return (
-    <Navbar expand="sm" bg="dark" variant="dark">
-      <Navbar.Brand href="#home">
-        <h2>Abelard Bautista</h2>
-      </Navbar.Brand>
-      <Navbar.Text className="mr-auto">{`abelardbautista@gmail.com ● (619) 549 7683`}</Navbar.Text>
-      <Nav>
-        <Nav.Link href="#about">About</Nav.Link>
-        {/* <Navbar.Collapse id="basic-navbar-nav">
+    <div>
+      <Navbar expand="sm" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          <h2>Abelard Bautista</h2>
+        </Navbar.Brand>
+        <Navbar.Text className="mr-auto">{`abelardbautista@gmail.com ● (619) 549 7683`}</Navbar.Text>
+        <Nav variant="pills">
+          <Nav.Link href="/About">About</Nav.Link>
+          {/* <Navbar.Collapse id="basic-navbar-nav">
           <Nav>
-            <NavDropdown title="Projects" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Project 1</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Project 2</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Project 3</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">Proejct 4</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.5">Proejct 5</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.6">Proejct 6</NavDropdown.Item>
-            </NavDropdown>
+          <NavDropdown title="Projects" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Project 1</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Project 2</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Project 3</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.4">Proejct 4</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.5">Proejct 5</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.6">Proejct 6</NavDropdown.Item>
+          </NavDropdown>
           </Nav>
         </Navbar.Collapse> */}
-        <Nav.Link href="#projects">Projects</Nav.Link>
-        <Nav.Link href="#resume">Resume</Nav.Link>
-      </Nav>
-    </Navbar>
+          <Nav.Link href="/Projects">Projects</Nav.Link>
+          <Nav.Link href="/Resume">Resume</Nav.Link>
+        </Nav>
+      </Navbar>
+      <Switch>
+        <Route path="/About">
+          <About />
+        </Route>
+        <Route path="/Projects">
+          <Projects />
+        </Route>
+        <Route path="/Resume">
+          <Resume />
+        </Route>
+      </Switch>
+    </div>
   );
 };
 
