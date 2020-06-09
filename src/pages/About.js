ba96038d3dd5d4b0d214a43c0cf4b79839ba6a8b
Abelard Bautista <abelardbautista@gmail.com>
2020-06-08 23:35:19 -0700

    Consolidated react bootstrap importing.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,5 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Image from "react-bootstrap/Image";
-import Button from "react-bootstrap/Button";
+import { Card, Container, Row, Col, Image, Button } from "react-bootstrap";
 
 const About = () => {
   return (
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { Container, Row, Col } from "react-bootstrap";
 import InOrOut from "../components/projectCards/InOrOut";
 import DayPlanner from "../components/projectCards/DayPlanner";
 import WeatherNow from "../components/projectCards/WeatherNow";
