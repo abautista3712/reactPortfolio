1a9bbfb77a174ed25a243fc27bf563040beb088f
Abelard Bautista <abelardbautista@gmail.com>
2020-06-15 02:09:03 -0700

    Added banner for thumbnail.

--- src/components/projectCards/Thumbnails/NoteTaker.js
+++ src/components/projectCards/Thumbnails/NoteTaker.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
+import { Card, Container, Row, Col } from "react-bootstrap";
 
 const NoteTaker = () => {
   return (
@@ -7,7 +7,35 @@ const NoteTaker = () => {
       <Card.Img
         src="./assets/images/resized/noteTaker3by2.png"
         alt="Card image with banner"
+        style={{ filter: "brightness(0.8)" }}
       />
+      <Card.ImgOverlay className="px-0 centerFlex">
+        <Card className="text-center thumbBanner">
+          <Card.Text className="my-auto" style={{ backgroundColor: "black" }}>
+            <h3>Note Taker</h3>
+          </Card.Text>
+          <Card.Text className="my-auto">
+            <Container>
+              <Row>
+                <Col
+                  xs={6}
+                  sm={6}
+                  md={4}
+                  lg={4}
+                  xl={4}
+                  style={{ fontSize: 13 }}
+                  className="centerFlex"
+                >
+                  Tools Utilized:
+                </Col>
+                <Col style={{ fontSize: 12 }} className="centerFlex">
+                  Node.js, Express
+                </Col>
+              </Row>
+            </Container>
+          </Card.Text>
+        </Card>
+      </Card.ImgOverlay>
     </Card>
   );
 };
