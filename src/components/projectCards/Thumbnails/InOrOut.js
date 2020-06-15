f96602e1c8d362046482759aa5ad658ec0272750
Abelard Bautista <abelardbautista@gmail.com>
2020-06-15 00:32:12 -0700

    Added styled banner for In or Out thumbnail.

--- src/components/projectCards/Thumbnails/InOrOut.js
+++ src/components/projectCards/Thumbnails/InOrOut.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import Card from "react-bootstrap/Card";
+import React from "react";
+import { Card, Container, Row, Col } from "react-bootstrap";
 
 const InOrOutThumb = () => {
   return (
@@ -8,6 +8,33 @@ const InOrOutThumb = () => {
         src="./assets/images/resized/inOrOut3by2.jpg"
         alt="Card image with banner"
       />
+      <Card.ImgOverlay className="px-0 centerFlex">
+        <Card className="text-center thumbBanner">
+          <Card.Text className="my-auto">
+            <h4>In or Out</h4>
+          </Card.Text>
+          <Card.Text className="my-auto">
+            <Container>
+              <Row>
+                <Col
+                  xs={4}
+                  sm={4}
+                  md={4}
+                  lg={4}
+                  xl={4}
+                  style={{ fontSize: 12 }}
+                >
+                  Tools Utilized:
+                </Col>
+                <Col style={{ fontSize: 10 }}>
+                  HTML, CSS, Materialize, JavaScript, jQuery, Spoonacular API,
+                  Google Maps API
+                </Col>
+              </Row>
+            </Container>
+          </Card.Text>
+        </Card>
+      </Card.ImgOverlay>
     </Card>
   );
 };
