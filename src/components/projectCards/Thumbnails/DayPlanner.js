e4fba010d49c42e54e1e7c42ce35e6ddf5918d0c
Abelard Bautista <abelardbautista@gmail.com>
2020-06-15 01:44:21 -0700

    Added banner to Day Planner thumbnail.

--- src/components/projectCards/Thumbnails/DayPlanner.js
+++ src/components/projectCards/Thumbnails/DayPlanner.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
+import { Card, Container, Row, Col } from "react-bootstrap";
 
 const DayPlanner = () => {
   return (
@@ -7,7 +7,34 @@ const DayPlanner = () => {
       <Card.Img
         src="./assets/images/resized/dayPlanner3by2.png"
         alt="Card image with banner"
+        style={{ filter: "brightness(0.5)" }}
       />
+      <Card.ImgOverlay className="px-0 centerFlex">
+        <Card className="text-center thumbBanner">
+          <Card.Text className="my-auto" style={{ backgroundColor: "black" }}>
+            <h3>Day Planner</h3>
+          </Card.Text>
+          <Card.Text className="my-auto">
+            <Container>
+              <Row>
+                <Col
+                  xs={6}
+                  sm={6}
+                  md={4}
+                  lg={4}
+                  xl={4}
+                  style={{ fontSize: 13 }}
+                >
+                  Tools Utilized:
+                </Col>
+                <Col style={{ fontSize: 10 }}>
+                  HTML, CSS, JavaScript, jQuery, Moment.js API
+                </Col>
+              </Row>
+            </Container>
+          </Card.Text>
+        </Card>
+      </Card.ImgOverlay>
     </Card>
   );
 };
